5de4ba4b1b714882cb7df28e0d28682f0d4a7734
Mani678 <84800961+Mani678@users.noreply.github.com>
2026-06-24 10:44:50 +0100

    Use correct sui import path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@
 import { ReactNode } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit'
-import { getFullnodeUrl } from '@mysten/sui'
+import { getFullnodeUrl } from '@mysten/sui/client'
 import '@mysten/dapp-kit/dist/index.css'
 import './globals.css'
 
